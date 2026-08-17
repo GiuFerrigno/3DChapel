c6c541055ecdd05e670da33daed6b2b1d187d791
GiuFerrigno <ferrigno.giulia02@gmail.com>
2026-08-17 16:25:50 +0200

    versione funzionante

--- project/src/chapel.js
+++ project/src/chapel.js
@@ -174,6 +174,8 @@ function drawChapel(view, projection, cameraPosition) {
   drawPart(chapelParts.roofCurved);
   drawPart(chapelParts.door);
 
+// TODO: Capire questa cosa della trasparenza
+  /*
   gl.enable(gl.BLEND);
   gl.blendFunc(
     gl.SRC_ALPHA,
@@ -181,13 +183,16 @@ function drawChapel(view, projection, cameraPosition) {
   );
   gl.depthMask(false);
   gl.disable(gl.CULL_FACE);
+  */
 
   drawPart(chapelParts.window);
   drawPart(chapelParts.windowLeft);
   drawPart(chapelParts.windowRight);
   drawPart(chapelParts.circWindow);
 
+  /*
   gl.enable(gl.CULL_FACE);
   gl.depthMask(true);
   gl.disable(gl.BLEND);
+  */
 }
